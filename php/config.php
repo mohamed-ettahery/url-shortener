42d8566ec833adfb013b70ca63e9ae2513230f78
Mohamed <ettaherymohamed.dev@gmail.com>
2024-03-07 14:19:26 +0100

    add all files

---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php 
+    $domain = "localhost/URL-Shortener/";
+    $host = "localhost";
+    $user = "root"; //Database username
+    $pass = ""; //Database password
+    $db = "urlShortener"; //Database name
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    if(!$conn){
+        echo "Database connection error".mysqli_connect_error();
+    }
+?>
